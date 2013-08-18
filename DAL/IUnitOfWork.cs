e30f238d621badceddbaa105a02ae0f27974e7dd
palesha <princeraj326@gmail.com>
2013-08-18 08:22:55 +0545

    Test

---
+++ DAL/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        int SaveChanges();
+    }
+
+    public interface IManageUnitOfWork : IUnitOfWork
+    {        
+        
+    }
+
+   
+}
